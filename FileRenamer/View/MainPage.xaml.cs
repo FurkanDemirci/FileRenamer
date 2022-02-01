46f755739071e8ab03004dce2d4f3fa5e4f6de6f
FurkanDemirci <furkan.demirci@live.nl>
2022-02-01 22:40:06 +0100

    Added MVVM structure and MainPage.

---
+++ FileRenamer/View/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using FileRenamer.Model;
+using FileRenamer.ViewModel;
+using System;
+using System.Diagnostics;
+using System.Windows.Controls;
+
+namespace FileRenamer.View
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        private readonly FileViewModel fileViewModel;
+
+        public MainPage()
+        {
+            fileViewModel = new FileViewModel();
+            DataContext = fileViewModel;
+            InitializeComponent();
+        }
+    }
+}
---
+++ FileRenamer/ViewModel/FileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using FileRenamer.Model;
+using FileRenamer.ViewModel.Binding;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace FileRenamer.ViewModel
+{
+    public class FileViewModel : ViewModelBase
+    {
+        public FileViewModel()
+        {
+            // In progress
+        }
+    }
+}
